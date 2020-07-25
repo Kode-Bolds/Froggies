7ebbd51cd406fed3b92e42c8bcaa7613654d78d1
Dean <deanb1996@hotmail.co.uk>
2020-07-25 13:55:55 +0100

    Add debug renderer with line drawing functionality.

--- Assets/Scripts/Systems/UnitMoveSystem.cs
+++ Assets/Scripts/Systems/UnitMoveSystem.cs
@@ -4,14 +4,16 @@ using Unity.Transforms;
 using Unity.Physics;
 using Unity.Mathematics;
 using UnityEngine;
+using Unity.Collections;
 
 public class UnitMoveSystem : KodeboldJobSystem
 {
 	private EndSimulationEntityCommandBufferSystem m_endSimECBSystem;
+	private DebugDrawer m_debugDrawer;
 
 	public override void GetSystemDependencies(Dependencies dependencies)
 	{
-
+		m_debugDrawer = dependencies.GetDependency<DebugDrawer>();
 	}
 
 	public override void InitSystem()
@@ -25,14 +27,36 @@ public class UnitMoveSystem : KodeboldJobSystem
 
 		float deltaTime = Time.DeltaTime;
 
-		Dependency = Entities.WithReadOnly(targetableByAILookup).ForEach((ref PhysicsVelocity velocity, in LocalToWorld transform, in UnitMove unitMove, in CurrentTarget currentTarget) =>
-		{
-			if (!targetableByAILookup.Exists(currentTarget.targetData.targetEntity))
-				return;
+#if UNITY_EDITOR
+		Dependency = JobHandle.CombineDependencies(Dependency, m_debugDrawer.debugDrawDependencies);
 
-			velocity.Linear = math.normalize(currentTarget.targetData.targetPos - transform.Position) * unitMove.moveSpeed * deltaTime;
-			velocity.Linear.y = 0;
-		}).ScheduleParallel(Dependency);
+		NativeQueue<DebugDrawCommand>.ParallelWriter debugDrawCommandQueue = m_debugDrawer.DebugDrawCommandQueue.AsParallelWriter();
+#endif
+		Dependency = Entities
+			.WithReadOnly(targetableByAILookup)
+			.ForEach((ref PhysicsVelocity velocity, in LocalToWorld transform, in UnitMove unitMove, in CurrentTarget currentTarget) =>
+			{
+				if (!targetableByAILookup.Exists(currentTarget.targetData.targetEntity))
+					return;
+#if UNITY_EDITOR
+				debugDrawCommandQueue.Enqueue(new DebugDrawCommand
+				{
+					debugDrawCommandType = DebugDrawCommandType.Line,
+					debugDrawLineData = new DebugDrawLineData
+					{
+						colour = Color.green,
+						start = transform.Position,
+						end = currentTarget.targetData.targetPos
+					}
+				});
+#endif
+				velocity.Linear = math.normalize(currentTarget.targetData.targetPos - transform.Position) * unitMove.moveSpeed * deltaTime;
+				velocity.Linear.y = 0;
+			}).ScheduleParallel(Dependency);
+
+#if UNITY_EDITOR
+		m_debugDrawer.debugDrawDependencies = Dependency;
+#endif
 
 		EntityCommandBuffer.Concurrent ecb = m_endSimECBSystem.CreateCommandBuffer().ToConcurrent();
 
