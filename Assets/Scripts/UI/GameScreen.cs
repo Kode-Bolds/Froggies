62014355a094fb04a8f3e2dce5f48e72dcd411a8
Tony Beacock <tonybeacock@hotmail.co.uk>
2020-10-19 21:34:27 +0100

    Added more to UI - Playing about with controlling from script

---
+++ Assets/Scripts/UI/GameScreen.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+[RequireComponent(typeof(UIDocument))]
+public class GameScreen : MonoBehaviour
+{
+    private VisualElement bottomPanel;
+    private VisualElement topPanel;
+
+    private Label resource1Text;
+    private Label resource2Text;
+    private Label resource3Text;
+
+    public int resource1Value = 100;
+    public int resource2Value = 100;
+    public int resource3Value = 100;
+
+
+    public VisualElement unitGrid;
+    public VisualElement buttonGrid;
+    public VisualElement mainUnit;
+
+
+    void OnEnable()
+    {
+        var rootVisualElement = GetComponent<UIDocument>().rootVisualElement;
+
+        bottomPanel = GetComponent<UIDocument>().rootVisualElement.Q("BottomBar");
+        topPanel = GetComponent<UIDocument>().rootVisualElement.Q("ResourceBar");
+
+        // Resource Text
+        resource1Text = rootVisualElement.Q<Label>("Resource1Text");
+        resource1Text.text = resource1Value.ToString();
+        resource2Text = rootVisualElement.Q<Label>("Resource2Text");
+        resource2Text.text = resource2Value.ToString();
+        resource3Text = rootVisualElement.Q<Label>("Resource3Text");
+        resource3Text.text = resource3Value.ToString();
+
+        // Selected Units Test
+        unitGrid = GetComponent<UIDocument>().rootVisualElement.Q("UnitGrid");
+        for (int i = 0; i < 9; i++)
+        {
+            AddToSelectedUnits();
+        }
+
+        // Buttons Test
+        buttonGrid = GetComponent<UIDocument>().rootVisualElement.Q("ButtonGrid");
+        for (int i = 0; i < 9; i++)
+        {
+            AddToButtons();
+        }
+
+        // Main Unit Test
+
+        // hoveredUnit = TODO: implemented when an onHover is implemented on a UI "unit"
+        mainUnit = GetComponent<UIDocument>().rootVisualElement.Q("MainUnit");
+
+    }
+
+
+    public void AddToSelectedUnits()
+    {
+        VisualElement unit = new VisualElement();
+        unit.AddToClassList("unit");
+
+        //unit.style.backgroundImage = 
+
+        unitGrid.Add(unit);
+    }
+
+    public void AddToButtons()
+    {
+        Button button = new Button();
+        button.AddToClassList("button");
+        button.text = "Tony";
+        // button.AddToClassList("unity-text-element");
+        // button.AddToClassList("unity-button");
+        // button.SetEnabled(false);
+
+        buttonGrid.Add(button);
+    }
+
+    public void HideBottomPanel()
+    {
+        bottomPanel.style.display = DisplayStyle.None;
+    }
+
+    public void HideTopPanel()
+    {
+        topPanel.style.display = DisplayStyle.None;
+    }
+}
